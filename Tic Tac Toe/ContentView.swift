c4c58c79dac7d1eb2df4697f40e7a721bff2c7e6
Lina Bhatia <lina@bhatia.org>
2022-07-22 12:59:21 -0500

    completed checkforwinner function

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -14,53 +14,64 @@ struct ContentView: View {
     @State private var winMessage = ""
     var body: some View {
         VStack{
-        Text("Tic Tac Toe")
-            .font(.title)
-            .fontWeight(.bold)
-            .padding()
+            Text("Tic Tac Toe")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15, content: {
                 ForEach(0..<9){
                     index in
                     ZStack{
-                    Color.blue
-                    Color.white
+                        Color.blue
+                        Color.white
                             .opacity(moves[index] == "" ? 1 : 0)
                         Text(moves[index])
                             .font(.system(size: 90))
                             .fontWeight(.heavy)
                     }
-                        .frame(width: 120, height: 120, alignment: .center)
-                        .cornerRadius(30)
-                        .onTapGesture {
-                            withAnimation(Animation.default){
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(30)
+                    .onTapGesture {
+                        withAnimation(Animation.default){
                             if moves[index] == ""{
-                            moves[index] = xTurn ? "X": "O"
-                            xTurn.toggle()
+                                moves[index] = xTurn ? "X": "O"
+                                xTurn.toggle()
                             }
                         }
-                        }
-                        .rotation3DEffect(.init(degrees: moves[index] != "" ? 180 : 0), axis: (x: 0.0, y: 1.0, z: 0.0))
-
+                    }
+                    .rotation3DEffect(.init(degrees: moves[index] != "" ? 180 : 0), axis: (x: 0.0, y: 1.0, z: 0.0))
+                    
                 }
-              
+                
             })
-    }
+        }
         .preferredColorScheme(.dark)
         .alert(isPresented: $gameOver){
             Alert(title: Text(winMessage))
         }
         .onChange(of: moves) {newValue in checkForWinner()}
-
+        
     }
-
+    
     
     private func checkForWinner(){
-        if moves[0] != "" && moves[0] == moves[1] && moves[1] == moves[2]{
-            winMessage = "\(moves[0]) is the winner"
+        checkLine(a: 0, b: 1, c: 2)
+        checkLine(a: 3, b: 4, c: 5)
+        checkLine(a: 6, b: 7, c: 8)
+        checkLine(a: 0, b: 3, c: 6)
+        checkLine(a: 1, b: 4, c: 7)
+        checkLine(a: 2, b: 5, c: 8)
+        
+        
+    }
+    private func checkLine(a: Int, b: Int, c: Int){
+        if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c]{
+            winMessage = "\(moves[a]) is the winner"
             gameOver = true
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
